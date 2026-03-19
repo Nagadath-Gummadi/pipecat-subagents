3d1d348f0dc4ea9ed38dc011f8478cea493485e3
Aleix Conchillo Flaqué <aleix@daily.co>
2026-03-18 21:27:13 -0700

    Move registry to registry/ package

---
+++ src/pipecat_subagents/registry/__init__.py
@@ -0,0 +1,13 @@
+#
+# Copyright (c) 2026, Daily
+#
+# SPDX-License-Identifier: BSD 2-Clause License
+#
+
+"""Agent registry for tracking discovered agents across runners."""
+
+from pipecat_subagents.registry.registry import AgentRegistry
+
+__all__ = [
+    "AgentRegistry",
+]
